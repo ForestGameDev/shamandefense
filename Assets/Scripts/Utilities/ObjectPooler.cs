c313d2ecb00fbd82345fbb1230508d5f97d2f9ea
marlon <marlon@karaokulta.com>
2016-01-29 23:31:08 -0600

    +Adding object pooler and twitch

---
+++ Assets/Scripts/Utilities/ObjectPooler.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class ObjectPooler {
+    /// <summary>
+    /// Prefab of object that will be pooled.
+    /// </summary>
+    public GameObject _prefab;
+    /// <summary>
+    /// Parent transform of the new poolable objects.
+    /// </summary>
+	public Transform _parent;
+    /// <summary>
+    /// Intantiate this number of objects at the start.
+    /// </summary>
+	public uint _poolLenght = 5;
+
+    List<GameObject> _poolList= new List<GameObject>();
+
+
+    /// <summary>
+    /// Prefab and parent must be set before initialize. 
+    /// </summary>
+    public void Initialize()
+	{
+        if(_prefab == null)
+        {
+            Debug.LogError("ObjectPooler missing prefab");
+            return;
+        }
+		for (int i = 0; i<_poolLenght; ++i) {
+			CreateObject();
+		}
+	}
+
+
+    public void Initialize(GameObject prefab)
+    {
+        this._prefab = prefab;
+        Initialize();
+    }
+    /// <summary>
+    /// Get an available object or create one. 
+    /// </summary>
+     public GameObject GetObject()
+    {
+
+        for (int i = 0; i < _poolList.Count; ++i)
+        {
+            if (!_poolList[i].activeInHierarchy)
+            {
+                return _poolList[i];
+            }
+            
+        }
+        int index = _poolList.Count;
+        if(_prefab == null)
+        {
+            return null;
+        }
+        CreateObject();
+        return _poolList[index];
+    }
+
+    public void DestroyPool()
+    {
+        for (int i = _poolList.Count - 1; i >= 0; --i)
+        {
+            GameObject.Destroy(_poolList[i]);
+        }
+        _poolList.Clear();
+    }
+
+    void CreateObject()
+	{
+		GameObject go = GameObject.Instantiate (_prefab) as GameObject;
+        go.SetActive (false);
+
+        if (_parent != null) {
+			go.transform.parent = _parent;
+            go.transform.position = go.transform.parent.position;
+        }
+		
+		_poolList.Add (go);
+
+	}
+
+
+
+}
---
+++ Assets/Scripts/Utilities/PoolDictionary.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PoolDictionary {
+
+    public Transform targetParent;
+    Dictionary<GameObject, ObjectPooler> dictionary = new Dictionary<GameObject, ObjectPooler>();
+
+    public GameObject Get(GameObject type,Transform target, bool autoActive = true)
+    {
+        GameObject go = Get(type);
+        if(go != null)
+        {
+            go.transform.position = target.position;
+            go.transform.rotation = target.rotation;
+            go.SetActive(autoActive);
+        }
+        return go;
+    }
+
+
+    public GameObject Get(GameObject type)
+    {
+        if(!dictionary.ContainsKey(type))
+        {
+            ObjectPooler pool = new ObjectPooler();
+            pool._parent = targetParent;
+            pool._prefab = type;
+            pool._poolLenght = 1;
+            pool.Initialize();
+            dictionary.Add(type, pool);
+        }
+        return dictionary[type].GetObject();
+    }
+
+    public bool Clear(GameObject type)
+    {
+        if(dictionary.ContainsKey(type))
+        {
+            dictionary[type].DestroyPool();
+            dictionary.Remove(type);
+            return true;
+        }
+        return false;
+    }
+
+}
