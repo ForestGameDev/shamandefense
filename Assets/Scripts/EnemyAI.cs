d986167629db9761666c644e5cf56cd28f15edc0
marlon <marlon@karaokulta.com>
2016-01-30 02:54:13 -0600

    +Adding prefabs, +Adding Enemy animation controller. +Adding. Bezier splines. +Adding Adding trees and blank map. !In Marlon Test there is an example of spline path.

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+
+public class EnemyAI : MonoBehaviour {
+
+    public    BezierSpline spline;
+    public   float duration;
+
+    UnityAction updateState;
+
+    public Transform exitLocation;
+    public float exitDistance = 1.0f;
+    public float pathDifference = .3f;
+
+    [Header("Debug")]
+    [SerializeField]    float deadTime = 1.0f;
+    [SerializeField]    float multiplier;
+
+    Animator animator;
+    private float progress = 0;
+
+
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        if(animator == null)
+        {
+            Debug.LogError("EnemyAi need an animator");
+            animator.SetTrigger("Reset");
+        }
+    }
+
+    void OnEnable()
+    {
+        progress = 0;
+        updateState = WalkingPathState;
+        multiplier = Random.Range(-pathDifference, pathDifference);
+    }
+
+    private void Update()
+    {
+        updateState();
+    }
+
+
+    void WalkingPathState()
+    {
+        progress += Time.deltaTime / duration;
+        if (progress > 1f)
+        {
+            progress = 1f;
+            updateState = KidnapingState;
+            animator.SetTrigger("Kidnap");
+            
+        }
+        Vector3 position = spline.GetPoint(progress);
+        transform.position = position + Quaternion.Euler(0, 0, 90) * spline.GetDirection(progress) * multiplier ;
+
+    }
+
+    void KidnapingState()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, exitLocation.position, Time.deltaTime);
+        float distance = Vector3.Distance(transform.position, exitLocation.position);
+        if (exitDistance >= distance)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void OnDead()
+    {
+        animator.SetTrigger("Dead");
+        StartCoroutine(WaitAndDisable());
+
+    }
+
+    IEnumerator WaitAndDisable()
+    {
+
+        yield return new WaitForSeconds(deadTime);
+        gameObject.SetActive(false);
+    }
+
+
+
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField] GameObject[] enemies;
+    [SerializeField] BezierSpline[] paths;
+    [SerializeField]    float timer = .1f;
+    [SerializeField]    Transform exitLocation;
+
+    PoolDictionary enemyPool = new PoolDictionary();
+
+
+
+	// Use this for initialization
+	void Start () {
+        enemyPool.targetParent = transform;
+        InvokeRepeating("Spawn", .01f, timer);
+
+    }
+
+
+    void Spawn()
+    {
+        int value = Random.Range(0, enemies.Length);
+        EnemyAI ai = enemyPool.Get(enemies[value]).GetComponent<EnemyAI>();
+        value = Random.Range(0, paths.Length);
+        ai.spline = paths[value];
+        ai.exitLocation = transform;
+        ai.gameObject.SetActive(true);
+
+    }
+}
