26556886fc767ab4e1e1b33da1c31333c926cd73
Juan Calvillo <juanfracalvillo@gmail.com>
2016-01-30 11:06:12 -0600

    Changes in input to select tower and put runes sprites

---
+++ Assets/Scripts/Utilities/VisibilityManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class VisibilityManager : MonoBehaviour {
+
+    public bool isVisible = true;
+    private bool wasVisible = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isVisible != wasVisible)
+        {
+            SetVisibility(this.gameObject);
+            wasVisible = isVisible;
+        }
+    }
+
+    private void SetVisibility(GameObject obj)
+    {
+        foreach (Transform child in obj.GetComponentsInChildren<Transform>())
+        {
+            Renderer childRenderer = child.GetComponent<Renderer>();
+            if (childRenderer != null)
+                childRenderer.enabled = isVisible;
+        }
+        Renderer theRenderer = obj.GetComponent<Renderer>();
+        if (theRenderer != null)
+            theRenderer.enabled = isVisible;
+    }
+}
