ee5b801342e0f3e1e00799eae7323423c323782e
marlon <marlon@karaokulta.com>
2016-01-30 17:37:22 -0600

    +Adding burnable forest.

---
+++ Assets/Scripts/BurnableForest.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+
+public class BurnableForest : MonoBehaviour {
+
+    // Use this for initialization
+    public delegate void EventInt(int  index);
+
+    static public EventInt EventOnBurn;
+
+    [SerializeField]   int index = -1;
+
+    void OnEnable()
+    {
+        EventOnBurn += OnBurn;
+    }
+
+    void OnDisable()
+    {
+        EventOnBurn -= OnBurn;
+    }
+
+        	
+    public void OnBurn(int index)
+    {
+        if(this.index == index)
+        {
+            StartCoroutine(BurnCoroutine());
+        }
+        
+    }
+
+    IEnumerator BurnCoroutine()
+    {
+        foreach (Transform child in transform)
+        {
+            yield return new WaitForSeconds(.1f);
+            child.gameObject.SetActive(false);
+        }
+        gameObject.SetActive(false);
+    }
+}
