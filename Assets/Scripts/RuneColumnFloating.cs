e9f50d4dad50494321e2cb82f756326ba0d86b23
marlon <marlon@karaokulta.com>
2016-01-31 12:21:54 -0600

    +Adding floating to the rune columns. +Avoid issue with the villager.

---
+++ Assets/Scripts/RuneColumnFloating.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RuneColumnFloating : MonoBehaviour {
+
+
+    [SerializeField] float distance;
+    [SerializeField] float time;
+
+    float randomSeed;
+    // Use this for initialization
+    Vector3 startPos;
+    void Start()
+    {
+        startPos = transform.position;
+        randomSeed = Random.value;
+    }
+
+    void Update()
+    {
+        Vector3 pos = Vector3.zero;
+        pos.y = Mathf.PingPong(Time.time + randomSeed, time) * distance;
+        transform.position = startPos + pos;
+    }
+	
+
+}
